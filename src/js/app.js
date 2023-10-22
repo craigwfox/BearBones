e0b0a342bdb95b66620b2dd36df3f52f080029a7
Craig Fox <craig@craigwfox.com>
2023-10-22 18:07:25 -0500

    renamed page nav

--- src/js/app.js
+++ src/js/app.js
@@ -16,17 +16,17 @@ const windowWidth = window.innerWidth
 
 // ===------ Convert rems to px ------===
 function remToPx(rem) {
-  const remSize = parseFloat(
-    getComputedStyle(document.documentElement).fontSize
-  )
-  const remClean = parseInt(rem.replace("rem", ""))
+	const remSize = parseFloat(
+		getComputedStyle(document.documentElement).fontSize
+	)
+	const remClean = parseInt(rem.replace("rem", ""))
 
-  return remClean * remSize
+	return remClean * remSize
 }
 
 // ===------ Get computed prop value ------===
 function getCompProp(compProp) {
-  return getComputedStyle(body).getPropertyValue(compProp)
+	return getComputedStyle(body).getPropertyValue(compProp)
 }
 
 // ====---------------====
@@ -34,112 +34,114 @@ function getCompProp(compProp) {
 // ====---------------====
 const headerHeight = document.querySelector("#page-header").offsetHeight
 if (headerHeight && body) {
-  body.style.setProperty("--header-height", `${headerHeight}px`)
+	body.style.setProperty("--header-height", `${headerHeight}px`)
 }
 
 // ====---------------====
 // Burger menu
 // ====---------------====
-const burgerWrapper = document.querySelector(".burger-wrapper")
+const burgerWrapper = document.querySelector(".page-nav-wrapper")
 if (burgerWrapper) {
-  const burgerList = burgerWrapper.querySelector(".burger-menu")
-  const burgerToggle = burgerWrapper.querySelector(".burger-btn")
+	const burgerList = burgerWrapper.querySelector(".page-nav")
+	const burgerToggle = burgerWrapper.querySelector(".page-nav-btn")
 
-  // ====---------------====
-  // Mobile burger toggle
-  // ====---------------====
-  burgerToggle.addEventListener("click", () => {
-    if (windowWidth <= remToPx(getCompProp("--tablet-min"))) {
-      const navState = burgerWrapper.getAttribute("data-menu-status")
+	// ====---------------====
+	// Mobile burger toggle
+	// ====---------------====
+	burgerToggle.addEventListener("click", () => {
+		if (windowWidth <= remToPx(getCompProp("--tablet-min"))) {
+			const navState = burgerWrapper.getAttribute("data-menu-status")
 
-      if (navState === "closed") {
-        burgerWrapper.setAttribute("data-menu-status", "open")
-        burgerToggle.setAttribute("aria-expanded", "true")
-      } else {
-        burgerWrapper.setAttribute("data-menu-status", "closed")
-        burgerToggle.setAttribute("aria-expanded", "false")
-      }
-    }
-  })
+			if (navState === "closed") {
+				burgerWrapper.setAttribute("data-menu-status", "open")
+				burgerToggle.setAttribute("aria-expanded", "true")
+			} else {
+				burgerWrapper.setAttribute("data-menu-status", "closed")
+				burgerToggle.setAttribute("aria-expanded", "false")
+			}
+		}
+	})
 
-  // ====---------------====
-  // Tablet+ burger controls
-  // ====---------------====
-  const burgerControls = burgerList.querySelectorAll('[data-toggle="nav"]')
+	// ====---------------====
+	// Tablet+ burger controls
+	// ====---------------====
+	const burgerControls = burgerList.querySelectorAll(
+		'[data-toggle="nav"]'
+	)
 
-  // open burger menu functions
-  function openBurgerSubmenu(link) {
-    // make sure the function doesn't run unless on tablet or larger
-    if (windowWidth > remToPx(getCompProp("--tablet-min"))) {
-      // set a tag to expanded
-      link.setAttribute("aria-expanded", "true")
-    }
-  }
+	// open burger menu functions
+	function openBurgerSubmenu(link) {
+		// make sure the function doesn't run unless on tablet or larger
+		if (windowWidth > remToPx(getCompProp("--tablet-min"))) {
+			// set a tag to expanded
+			link.setAttribute("aria-expanded", "true")
+		}
+	}
 
-  // close burger menu functions
-  function closeBurgerSubmenu(link) {
-    // make sure the function doesn't run unless on tablet or larger
-    if (windowWidth > remToPx(getCompProp("--tablet-min"))) {
-      // set a tag to not expanded
-      link.setAttribute("aria-expanded", "false")
-    }
-  }
+	// close burger menu functions
+	function closeBurgerSubmenu(link) {
+		// make sure the function doesn't run unless on tablet or larger
+		if (windowWidth > remToPx(getCompProp("--tablet-min"))) {
+			// set a tag to not expanded
+			link.setAttribute("aria-expanded", "false")
+		}
+	}
 
-  // close all burger submenus
-  function closeAllBurgerSubmenus() {
-    burgerControls.forEach(control => {
-      const controlLink = control.querySelector(":scope > a")
-      closeBurgerSubmenu(controlLink)
-    })
-  }
+	// close all burger submenus
+	function closeAllBurgerSubmenus() {
+		burgerControls.forEach(control => {
+			const controlLink = control.querySelector(":scope > a")
+			closeBurgerSubmenu(controlLink)
+		})
+	}
 
-  burgerControls.forEach(control => {
-    const controlLink = control.querySelector(":scope > a")
+	burgerControls.forEach(control => {
+		const controlLink = control.querySelector(":scope > a")
 
-    // ===------ Mouse enter ------===
-    control.addEventListener("mouseenter", () => {
-      openBurgerSubmenu(controlLink)
-    })
+		// ===------ Mouse enter ------===
+		control.addEventListener("mouseenter", () => {
+			openBurgerSubmenu(controlLink)
+		})
 
-    // ===------ Mouse leave ------===
-    control.addEventListener("mouseleave", () => {
-      closeBurgerSubmenu(controlLink)
-    })
+		// ===------ Mouse leave ------===
+		control.addEventListener("mouseleave", () => {
+			closeBurgerSubmenu(controlLink)
+		})
 
-    // ===------ Focus in ------===
-    controlLink.addEventListener("focusin", () => {
-      closeAllBurgerSubmenus()
+		// ===------ Focus in ------===
+		controlLink.addEventListener("focusin", () => {
+			closeAllBurgerSubmenus()
 
-      // open the current burger submenu
-      openBurgerSubmenu(controlLink)
-    })
+			// open the current burger submenu
+			openBurgerSubmenu(controlLink)
+		})
 
-    // ===------ Focus out of control link backwards ------===
-    controlLink.addEventListener("focusout", () => {
-      // close menus if focus is not on a child of the control
-      setTimeout(() => {
-        if (!control.contains(document.activeElement)) {
-          closeBurgerSubmenu(controlLink)
-        }
-      }, 1)
-    })
+		// ===------ Focus out of control link backwards ------===
+		controlLink.addEventListener("focusout", () => {
+			// close menus if focus is not on a child of the control
+			setTimeout(() => {
+				if (!control.contains(document.activeElement)) {
+					closeBurgerSubmenu(controlLink)
+				}
+			}, 1)
+		})
 
-    // ===------ Watch for focus leaving last child in a dropdown or mega menu ------===
-    const controlChildren = control.querySelectorAll(":scope a")
-    const lastChild = controlChildren[controlChildren.length - 1]
-    lastChild.addEventListener("focusout", () => {
-      setTimeout(() => {
-        if (!control.contains(document.activeElement)) {
-          closeBurgerSubmenu(controlLink)
-        }
-      }, 1)
-    })
-  })
+		// ===------ Watch for focus leaving last child in a dropdown or mega menu ------===
+		const controlChildren = control.querySelectorAll(":scope a")
+		const lastChild = controlChildren[controlChildren.length - 1]
+		lastChild.addEventListener("focusout", () => {
+			setTimeout(() => {
+				if (!control.contains(document.activeElement)) {
+					closeBurgerSubmenu(controlLink)
+				}
+			}, 1)
+		})
+	})
 
-  // ===------ Escape to close burger submenus ------===
-  document.addEventListener("keydown", e => {
-    if (e.key === "Escape") {
-      closeAllBurgerSubmenus()
-    }
-  })
+	// ===------ Escape to close burger submenus ------===
+	document.addEventListener("keydown", e => {
+		if (e.key === "Escape") {
+			closeAllBurgerSubmenus()
+		}
+	})
 }
